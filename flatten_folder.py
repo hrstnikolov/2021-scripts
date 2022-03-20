24bec01e038861b07f5ec2816cd4627dd0c144aa
Nikolov <HNikolov@sensata.com>
2022-03-20 17:04:13 +0200

    up

---
+++ flatten_folder.py
@@ -0,0 +1,8 @@
+'''
+# windows command line script to flatten a directory
+
+for /r "Sorce folder" %f in (*) do @move "%f" "Destination Folder"
+for /r "C:\Users\a1056968\OneDrive - PROD - sensata.com Azure AD (sso.sensata.com) (Not O365)\lib\Python SoftUni\03_Python_OOP" %f in (*) do @move "%f" "C:\Users\a1056968\OneDrive - PROD - sensata.com Azure AD (sso.sensata.com) (Not O365)\lib\Python SoftUni\03_Python_OOP"
+'''
+
+# TODO write python script

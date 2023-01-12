e83fe355c0e0c151119c43bba71b22159a7881e6
hrstnikolov <64984527+hrstnikolov@users.noreply.github.com>
2023-01-12 11:34:52 +0200

    Create run_jupyter_lab.bat

---
+++ Batchfiles/run_jupyter_lab.bat
@@ -0,0 +1,4 @@
+call conda activate machine_learning
+call jupyter lab
+pause
+:: to automatically minimize the cmd window create shortcut to .bat and update the "Run" field in the shortcut file properties
